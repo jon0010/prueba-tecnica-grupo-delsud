7fc32e4e493d0b8a7a1d2ade4d01bae781e775bf
jon0010 <jonnahuel78@gmail.com>
2024-01-24 18:42:33 -0300

    navbar layout completed

---
+++ app/ui/components/navbar/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Links } from "./links";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <Links />
+    </div>
+  );
+};
---
+++ app/ui/views/home/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Navbar } from "../../components/navbar/page";
+
+export const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>home</h1>
+    </div>
+  );
+};
