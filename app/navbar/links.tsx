0ad4289dc20a1016b4bf40d12dffc9a65f1d80a7
jon0010 <jonnahuel78@gmail.com>
2024-01-26 20:50:38 -0300

    implemented the character detail modal with link for each comic to the comic detail section with formatted data

--- app/navbar/links.tsx
+++ app/navbar/links.tsx
@@ -1,9 +1,13 @@
+"use client";
+import { useRouter } from "next/navigation";
 import NavLink from "next/link";
 import marvel from "../assets/marvel.png";
 import { RiStarLine } from "react-icons/ri";
 import Image from "next/image";
 
 export function Links() {
+  const router = useRouter();
+
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary row px-3 fw-semibold py-3 fs-5">
       <div className="container-fluid">
@@ -39,6 +43,7 @@ export function Links() {
         <button
           className="justify-content-end bg-body-tertiary"
           style={{ textDecoration: "none", border: "none" }}
+          onClick={() => router.push("/favoritos")}
         >
           Favoritos&nbsp;&nbsp;
           <RiStarLine className="fs-2 me-5 text-warning" />
