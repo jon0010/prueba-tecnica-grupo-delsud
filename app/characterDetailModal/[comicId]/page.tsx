9b5a82a33738f73f1c0762fce2f794926a4484e1
jon0010 <jonnahuel78@gmail.com>
2024-01-29 00:13:19 -0300

    loader implemented

--- app/characterDetailModal/[comicId]/page.tsx
+++ app/characterDetailModal/[comicId]/page.tsx
@@ -10,6 +10,7 @@ import { Bangers } from "next/font/google";
 import styles from "../../cardCharacter/card.module.css";
 import { formatDate, capitalizeFirstLetter } from "../../utils/formatDate";
 import { useStyles } from "../../setDarkMode";
+import Loader from "../../loader";
 
 const bangers = Bangers({ subsets: ["latin"], weight: "400" });
 
@@ -18,6 +19,7 @@ const ComicDetail = () => {
   const comicId = pathname.split("/").pop() || "";
   const [comicDetails, setComicDetails] = useState<IComic | null>(null);
   const { darkMode } = useStyles();
+  const [showLoader, setShowLoader] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,91 +35,107 @@ const ComicDetail = () => {
     fetchData();
   }, [comicId]);
 
+  useEffect(() => {
+    setShowLoader(true);
+    const timer = setTimeout(() => {
+      setShowLoader(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className={` ${darkMode ? "bg-dark" : "bg-light"}`}>
-      <Navbar />
-      <h1
-        className={`modal-title ${bangers.className} ${styles["titleComicDetail"]}  text-warning mb-4 text-center mt-2`}
-        style={{
-          textShadow: darkMode
-            ? "3px 2px 2px rgba(255, 255, 255, 1)"
-            : "3px 2px 2px rgba(0, 0, 0, 1)",
-        }}
+    <>
+      {showLoader && <Loader />}
+      <div
+        className={` ${darkMode ? "bg-dark" : "bg-light"}`}
+        style={{ position: showLoader ? "fixed" : "relative" }}
       >
-        comic book info
-      </h1>
-      {comicDetails && (
-        <div className={`row  ${darkMode ? "text-white" : "text-dark"}`}>
-          <div className="col-sm-12 col-md-6 text-center px-5">
-            <Image
-              className="img-fluid border border-2 border-danger rounded"
-              src={`${comicDetails.thumbnail.path}.${comicDetails.thumbnail.extension}`}
-              alt={comicDetails.title}
-              height={600}
-              width={600}
-            />
+        <Navbar />
+        <h1
+          className={`modal-title ${bangers.className} ${styles["titleComicDetail"]}  text-warning mb-4 text-center mt-2`}
+          style={{
+            textShadow: darkMode
+              ? "3px 2px 2px rgba(255, 255, 255, 1)"
+              : "3px 2px 2px rgba(0, 0, 0, 1)",
+          }}
+        >
+          comic book info
+        </h1>
+        {comicDetails && (
+          <div className={`row  ${darkMode ? "text-white" : "text-dark"}`}>
+            <div className="col-sm-12 col-md-6 text-center px-5">
+              <Image
+                className="img-fluid border border-2 border-danger rounded"
+                src={`${comicDetails.thumbnail.path}.${comicDetails.thumbnail.extension}`}
+                alt={comicDetails.title}
+                height={600}
+                width={600}
+              />
+            </div>
+            <div className="col-sm-12 col-md-6 px-5">
+              <h2 className={`${bangers.className}`}>{comicDetails.title}</h2>
+              <p>
+                Comments:{" "}
+                {comicDetails.variantDescription || "comments no available"}
+              </p>
+              <p>
+                Description:{" "}
+                {comicDetails.description || "description no available"}
+              </p>
+
+              <h3 className={`${bangers.className}`}>Creators:</h3>
+              {comicDetails.creators.items.map((creator, index) => (
+                <div key={index}>
+                  <span>
+                    <p>
+                      <span>{creator.name || "creators no available"}</span> 🧑‍💻{" "}
+                      <span>Role: {creator.role}</span>
+                    </p>
+                  </span>
+                </div>
+              ))}
+
+              <h3 className={`${bangers.className}`}>Dates:</h3>
+              {comicDetails.dates.map((date, index) => (
+                <div key={index}>
+                  <span>
+                    <p>
+                      <span>{formatDate(date.date)}</span>
+                      <span>&nbsp; {capitalizeFirstLetter(date.type)}</span>
+                    </p>
+                  </span>
+                </div>
+              ))}
+
+              <h3 className={`${bangers.className}`}>Prices:</h3>
+              {comicDetails.prices.map((price, index) => (
+                <div key={index}>
+                  <span>
+                    <p>
+                      <span>{capitalizeFirstLetter(price.type)}</span>
+                      <span>&nbsp;&nbsp;${price.price}</span>
+                    </p>
+                  </span>
+                </div>
+              ))}
+
+              <h3 className={`${bangers.className}`}>synopsis</h3>
+              {comicDetails.textObjects.length > 0 && (
+                <div>
+                  <p>Language: {comicDetails.textObjects[0].language}</p>
+                  <p>
+                    {comicDetails.textObjects[0].text ||
+                      "synopsis no available"}
+                  </p>
+                </div>
+              )}
+            </div>
           </div>
-          <div className="col-sm-12 col-md-6 px-5">
-            <h2 className={`${bangers.className}`}>{comicDetails.title}</h2>
-            <p>
-              Comments:{" "}
-              {comicDetails.variantDescription || "comments no available"}
-            </p>
-            <p>
-              Description:{" "}
-              {comicDetails.description || "description no available"}
-            </p>
-
-            <h3 className={`${bangers.className}`}>Creators:</h3>
-            {comicDetails.creators.items.map((creator, index) => (
-              <div key={index}>
-                <span>
-                  <p>
-                    <span>{creator.name || "creators no available"}</span> 🧑‍💻{" "}
-                    <span>Role: {creator.role}</span>
-                  </p>
-                </span>
-              </div>
-            ))}
-
-            <h3 className={`${bangers.className}`}>Dates:</h3>
-            {comicDetails.dates.map((date, index) => (
-              <div key={index}>
-                <span>
-                  <p>
-                    <span>{formatDate(date.date)}</span>
-                    <span>&nbsp; {capitalizeFirstLetter(date.type)}</span>
-                  </p>
-                </span>
-              </div>
-            ))}
-
-            <h3 className={`${bangers.className}`}>Prices:</h3>
-            {comicDetails.prices.map((price, index) => (
-              <div key={index}>
-                <span>
-                  <p>
-                    <span>{capitalizeFirstLetter(price.type)}</span>
-                    <span>&nbsp;&nbsp;${price.price}</span>
-                  </p>
-                </span>
-              </div>
-            ))}
-
-            <h3 className={`${bangers.className}`}>synopsis</h3>
-            {comicDetails.textObjects.length > 0 && (
-              <div>
-                <p>Language: {comicDetails.textObjects[0].language}</p>
-                <p>
-                  {comicDetails.textObjects[0].text || "synopsis no available"}
-                </p>
-              </div>
-            )}
-          </div>
-        </div>
-      )}
-      <Footer />
-    </div>
+        )}
+        <Footer />
+      </div>
+    </>
   );
 };
 
--- app/favoritos/page.tsx
+++ app/favoritos/page.tsx
@@ -9,6 +9,7 @@ import { Bangers } from "next/font/google";
 import Pagination from "../components/pagination/page";
 import styles from "../cardCharacter/card.module.css";
 import { useStyles } from "../setDarkMode";
+import Loader from "../loader";
 
 const bangers = Bangers({ subsets: ["latin"], weight: "400" });
 
@@ -20,12 +21,22 @@ const Favoritos = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const cardsPerPage = 8;
   const { darkMode } = useStyles();
+  const [showLoader, setShowLoader] = useState(true);
 
   useEffect(() => {
     const storedFavorites = JSON.parse(
       localStorage.getItem("favorites") || "[]"
     ) as ICharacter[];
     setFavorites(storedFavorites);
+  }, []);
+
+  useEffect(() => {
+    setShowLoader(true);
+    const timer = setTimeout(() => {
+      setShowLoader(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
   }, []);
 
   const handleCardClick = async (character: ICharacter) => {
@@ -54,78 +65,81 @@ const Favoritos = () => {
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
   return (
-    <div
-      className={`row px-0 ${darkMode ? "bg-dark" : "bg-light"}`}
-      style={{ height: "100%" }}
-    >
-      <Navbar />
-      <div className="col-2 col-sm-0 d-none d-sm-block"></div>
-      <div className="col-8 container-fluid p-0">
-        <h1
-          className={`modal-title ${bangers.className} ${styles["titleDetail"]} text-warning text-center my-5`}
-          style={{
-            textShadow: darkMode
-              ? "3px 2px 2px rgba(255, 255, 255, 1)"
-              : "3px 2px 2px rgba(0, 0, 0, 1)",
-          }}
-        >
-          favorites
-        </h1>
-        <div className="mb-4">
-          <Pagination
-            cardsPerPage={cardsPerPage}
-            totalCards={favorites.length}
-            currentPage={currentPage}
-            paginate={paginate}
-          />
-        </div>
-        <div className={`row justify-content-center ${styles["card-row"]}`}>
-          {favorites.map((character, index) => {
-            const lowercaseName = character.name.toLowerCase();
-            const isComic = lowercaseName.includes("comic");
-            return !isComic ? (
-              <div
-                key={index}
-                className={`col-12 col-sm-6 col-md-3 col-lg-3 ${styles["card-wrapper"]} ${styles["custom-breakpoint-class"]}`}
-                style={{ cursor: "pointer" }}
-                onClick={() => handleCardClick(character)}
-              >
+    <>
+      {showLoader && <Loader />}
+      <div
+        className={`row px-0 ${darkMode ? "bg-dark" : "bg-light"}`}
+        style={{ height: "100%", position: showLoader ? "fixed" : "relative" }}
+      >
+        <Navbar />
+        <div className="col-2 col-sm-0 d-none d-sm-block"></div>
+        <div className="col-8 container-fluid p-0">
+          <h1
+            className={`modal-title ${bangers.className} ${styles["titleDetail"]} text-warning text-center my-5`}
+            style={{
+              textShadow: darkMode
+                ? "3px 2px 2px rgba(255, 255, 255, 1)"
+                : "3px 2px 2px rgba(0, 0, 0, 1)",
+            }}
+          >
+            favorites
+          </h1>
+          <div className="mb-4">
+            <Pagination
+              cardsPerPage={cardsPerPage}
+              totalCards={favorites.length}
+              currentPage={currentPage}
+              paginate={paginate}
+            />
+          </div>
+          <div className={`row justify-content-center ${styles["card-row"]}`}>
+            {favorites.map((character, index) => {
+              const lowercaseName = character.name.toLowerCase();
+              const isComic = lowercaseName.includes("comic");
+              return !isComic ? (
                 <div
-                  className={`card mt-4 d-flex flex-row-reverse flex-wrap align-content-start justify-content-start align-items-center ${styles.card} ${styles["custom-animation-class"]}`}
-                  style={{
-                    backgroundImage: `url('${character.thumbnail.path}.${character.thumbnail.extension}')`,
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "cover",
-                    cursor: "pointer",
-                  }}
+                  key={index}
+                  className={`col-12 col-sm-6 col-md-3 col-lg-3 ${styles["card-wrapper"]} ${styles["custom-breakpoint-class"]}`}
+                  style={{ cursor: "pointer" }}
+                  onClick={() => handleCardClick(character)}
                 >
-                  <div className={`card-text ${styles["card-text"]}`}>
-                    <p
-                      className="d-flex justify-content-start ms-2"
-                      style={{ textShadow: "2px 2px 2px rgba(0, 0, 0, 1)" }}
-                    >
-                      {character.name}
-                    </p>
+                  <div
+                    className={`card mt-4 d-flex flex-row-reverse flex-wrap align-content-start justify-content-start align-items-center ${styles.card} ${styles["custom-animation-class"]}`}
+                    style={{
+                      backgroundImage: `url('${character.thumbnail.path}.${character.thumbnail.extension}')`,
+                      backgroundRepeat: "no-repeat",
+                      backgroundSize: "cover",
+                      cursor: "pointer",
+                    }}
+                  >
+                    <div className={`card-text ${styles["card-text"]}`}>
+                      <p
+                        className="d-flex justify-content-start ms-2"
+                        style={{ textShadow: "2px 2px 2px rgba(0, 0, 0, 1)" }}
+                      >
+                        {character.name}
+                      </p>
+                    </div>
+                    <span className="top spanCard"></span>
+                    <span className="right spanCard"></span>
+                    <span className="bottom spanCard"></span>
+                    <span className="left spanCard"></span>
                   </div>
-                  <span className="top spanCard"></span>
-                  <span className="right spanCard"></span>
-                  <span className="bottom spanCard"></span>
-                  <span className="left spanCard"></span>
                 </div>
-              </div>
-            ) : null;
-          })}
+              ) : null;
+            })}
+          </div>
         </div>
+        <div className="col-2 col-sm-0 d-none d-sm-block"></div>
+        {showModal && selectedCharacter && (
+          <CharacterModal
+            characterDetails={selectedCharacter}
+            onClose={handleCloseModal}
+          />
+        )}
+        <Footer />
       </div>
-      <div className="col-2 col-sm-0 d-none d-sm-block"></div>
-      {showModal && selectedCharacter && (
-        <CharacterModal
-          characterDetails={selectedCharacter}
-          onClose={handleCloseModal}
-        />
-      )}
-      <Footer />
-    </div>
+    </>
   );
 };
 
